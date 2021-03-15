c43db1f4df92de33d1901fa200724469d65ac2d2
Vybon <80249673+Vybon@users.noreply.github.com>
2021-03-15 16:13:54 +0700

    Update Form1.cs

--- Test/Test/Form1.cs
+++ Test/Test/Form1.cs
@@ -15,16 +15,19 @@ namespace Test
         public Form1()
         {
             InitializeComponent();
-            int tong = tonghaiso(2, 3);
             int tru = truhaiso(2, 3);
         }
         public int tonghaiso(int a, int b)
         {
             return a + b;
         }
-        public int truhaiso(int a, int b)
+        
+        
+        
+        
+        public int truhaiso(int x, int c)
         {
-            return a - b;
+            return x - c;
         }
         private void Form1_Load(object sender, EventArgs e)
         {

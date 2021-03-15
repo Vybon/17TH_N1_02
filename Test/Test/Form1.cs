7af943ba11de86fce80d34de7b39bca3897b7214
bon <bongamingbt@gmail.com>
2021-03-15 15:38:48 +0700

    tong hai so

--- Test/Test/Form1.cs
+++ Test/Test/Form1.cs
@@ -15,7 +15,15 @@ namespace Test
         public Form1()
         {
             InitializeComponent();
+            int tong = tonghaiso(2, 3);
         }
+        public int tonghaiso(int a, int b)
+        {
+            return a + b;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
+        }
     }
 }
